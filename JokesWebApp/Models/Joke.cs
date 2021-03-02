11cca5fd7df32421f6abd7bb7854f2384cb1a209
YXGuan <55643200+YXGuan@users.noreply.github.com>
2021-03-02 14:52:20 -0500

    Add project files.

---
+++ JokesWebApp/Models/Joke.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+/// <summary>
+/// added
+/// </summary>
+namespace JokesWebApp.Models
+{
+    public class Joke
+    {
+        //shortcut tap in prop
+        public int Id { get; set; }
+
+        // prop  tab twice
+        public String JokeQuestion { get; set; }
+        public String JokeAnswer  { get; set; }
+
+
+        //ctor tabtab
+        public Joke()
+        {
+             
+        }
+           
+    }
+}
